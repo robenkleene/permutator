8259c6cf7e93a3e7ae4cdaa3e0152d40b1df64ff
Roben Kleene <contact@robenkleene.com>
2025-01-28 06:47:25 -0500

    Write JavaScript to get parameters

--- javascript/permutator_parameters.js
+++ javascript/permutator_parameters.js
@@ -1,2 +1,18 @@
 inlets = 1;
 outlets = 1;
+
+var liveObject = new LiveAPI(callback, "live_set");
+
+function msg_int(value) {
+  liveObject.id = value;
+  var paramCount = liveObject.getcount("parameters");
+  var parameterNames = [];
+
+  for (var i = 0; i < paramCount; i++) {
+    var parameterPath = "parameters " + i;
+    var parameterName = liveObject.get(parameterPath + " name");
+    parameterNames.push(parameterName);
+  }
+
+  outlet(0, parameterNames);
+}
